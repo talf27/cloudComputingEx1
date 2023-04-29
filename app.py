43343422bf02391b67db65cb174a40af812d87c4
Tal Feldman <tal.feldman01@post.idc.ac.il>
2023-04-29 15:12:18 +0300

    first version

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+ticketId = 1
+parkedCarsInfo = {}
+
+@app.route('/entry', methods=['POST'])
+def entry():
+    plate = request.args.get('plate')
+    parkingLot = request.args.get('parkingLot')
+    parkedCarsInfo[ticketId] = (plate, parkingLot, datetime.now())
+    ticketId += 1
+
+    return f"your ticket ID is: {ticketId}. Enjoy!"
+
+@app.route('/exit')
+def exit():
+    requestedTicketId = request.args.get('ticketId')
+    carInfo = parkedCarsInfo[ticketId]
+    totalHours = (datetime.now() - carInfo[2]).total_seconds() / 3600
+    if totalHours - int(totalHours) < 0.25:
+        totalHours = int(totalHours)
+    else:
+        totalHours = int(totalHours) + 1
+    
+    return f"""License plate: {carInfo[0]}
+    total parked time: {totalHours} hours
+    parking lot ID: {carInfo[1]}
+    total charge: {totalHours * 10}$
+    Thank you :)"""
