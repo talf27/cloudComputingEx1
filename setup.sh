bfb9ef4d7099654ab0c454e67efb70559107cbe2
Tal Feldman <tal.feldman01@post.idc.ac.il>
2023-04-30 16:27:46 +0300

    setup script version 2

--- setup.sh
+++ setup.sh
@@ -1,69 +1,49 @@
-# debug
-# set -o xtrace
+#!/bin/bash
 
-KEY_NAME="cloud-course-`date +'%N'`"
+# Set variables
+KEY_NAME="cloud-course-$(date +'%N')"
 KEY_PEM="$KEY_NAME.pem"
-
-echo "create key pair $KEY_PEM to connect to instances and save locally"
-aws ec2 create-key-pair --key-name $KEY_NAME \
-    | jq -r ".KeyMaterial" > $KEY_PEM
-
-# secure the key pair
-chmod 400 $KEY_PEM
-
-SEC_GRP="my-sg-`date +'%N'`"
-
-echo "setup firewall $SEC_GRP"
-aws ec2 create-security-group   \
-    --group-name $SEC_GRP       \
-    --description "Access my instances" 
-
-# figure out my ip
-MY_IP=$(curl ipinfo.io/ip)
-echo "My IP: $MY_IP"
-
-
-echo "setup rule allowing SSH access to $MY_IP only"
-aws ec2 authorize-security-group-ingress        \
-    --group-name $SEC_GRP --port 22 --protocol tcp \
-    --cidr $MY_IP/32
-
-echo "setup rule allowing HTTP (port 5000) access to $MY_IP only"
-aws ec2 authorize-security-group-ingress        \
-    --group-name $SEC_GRP --port 5000 --protocol tcp \
-    --cidr $MY_IP/32
-
+SEC_GRP="my-sg-$(date +'%N')"
 UBUNTU_20_04_AMI="ami-042e8287309f5df03"
 
-echo "Creating Ubuntu 20.04 instance..."
-RUN_INSTANCES=$(aws ec2 run-instances   \
-    --image-id $UBUNTU_20_04_AMI        \
-    --instance-type t3.micro            \
-    --key-name $KEY_NAME                \
-    --security-groups $SEC_GRP)
+# Create key pair
+echo "Creating key pair $KEY_PEM to connect to instances and saving locally"
+aws ec2 create-key-pair --key-name $KEY_NAME \
+    | jq -r ".KeyMaterial" > $KEY_PEM
+chmod 400 $KEY_PEM
 
+# Create security group
+echo "Creating security group $SEC_GRP"
+aws ec2 create-security-group --group-name $SEC_GRP --description "Access my instances" 
+
+# Get the public IP address
+MY_IP=$(curl ipinfo.io/ip)
+
+# Set up firewall rules
+echo "Setting up firewall rules for SSH and HTTP access from $MY_IP only"
+aws ec2 authorize-security-group-ingress --group-name $SEC_GRP --port 22 --protocol tcp --cidr $MY_IP/32
+aws ec2 authorize-security-group-ingress --group-name $SEC_GRP --port 5000 --protocol tcp --cidr $MY_IP/32
+
+# Launch EC2 instance
+echo "Launching Ubuntu 20.04 instance"
+RUN_INSTANCES=$(aws ec2 run-instances --image-id $UBUNTU_20_04_AMI --instance-type t3.micro --key-name $KEY_NAME --security-groups $SEC_GRP)
 INSTANCE_ID=$(echo $RUN_INSTANCES | jq -r '.Instances[0].InstanceId')
 
+# Wait for the instance to be created
 echo "Waiting for instance creation..."
 aws ec2 wait instance-running --instance-ids $INSTANCE_ID
 
-PUBLIC_IP=$(aws ec2 describe-instances  --instance-ids $INSTANCE_ID | 
-    jq -r '.Reservations[0].Instances[0].PublicIpAddress'
-)
+# Get the public IP address of the instance
+PUBLIC_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID | jq -r '.Reservations[0].Instances[0].PublicIpAddress')
 
-echo "New instance $INSTANCE_ID @ $PUBLIC_IP"
-
-echo "deploying code to production"
+# Deploy the Flask app
+echo "Deploying Flask app to $PUBLIC_IP"
 scp -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=60" app.py ubuntu@$PUBLIC_IP:/home/ubuntu/
-
-echo "setup production environment"
 ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP <<EOF
     sudo apt update
     sudo apt install python3-flask -y
-    # run app
-    nohup flask run --host 0.0.0.0  &>/dev/null &
+    nohup flask run --host 0.0.0.0 &>/dev/null &
     exit
 EOF
 
-echo "test that it all worked"
-curl  --retry-connrefused --retry 10 --retry-delay 1  http://$PUBLIC_IP:5000
+echo "Deployment complete!"
